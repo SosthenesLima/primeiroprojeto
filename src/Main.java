471d401a08c37dc6a2743c38a7ad41731461333f
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-01-27 16:27:19 -0300

    Commit - Começando o projeto Screen Match
    Signed-off-by: Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Alô Mundo!!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLacamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLacamento);
+        boolean incluidoNoPlano = true;
 
 
     }
