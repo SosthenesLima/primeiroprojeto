ab9df0a83221604f345773720b0847361a304f1a
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-01-30 09:48:42 -0300

    Commit - Exercícios - 03 e 05
    Signed-off-by: Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>

--- src/Comparacao.java
+++ src/Comparacao.java
@@ -1,3 +1,7 @@
+/*
+Peça ao usuário para inserir dois números inteiros. Compare os números e imprima uma mensagem
+indicando se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
+ */
 import java.util.Scanner;
 
 public class Comparacao {
